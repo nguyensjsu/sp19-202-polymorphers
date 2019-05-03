e2538bfeb6c97d5a2f99f6c86483a2054fb429e2
Shivam Waghela <shivam.waghela@sjsu.edu>
2019-05-02 18:13:55 -0700

    fix highscore button

--- Project/EggCatcher/Scoreboard.java
+++ Project/EggCatcher/Scoreboard.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Scoreboard extends Actor implements IScoreObserver
 {
     public int score = 0;
-    public int highscore = 0;
+    public static int highscore = 0;
     /**
      * Act - do whatever the Scoreboard wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
